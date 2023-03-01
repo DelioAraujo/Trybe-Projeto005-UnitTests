ce85b3a691c8749be26dd9a90284d5d504be05aa
DelioAraujo <delioaraujo@gmail.com>
2023-03-01 10:57:23 -0300

    linter

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -35,12 +35,10 @@
 */
 
 const calculator = (number1, number2) => ({
-  return {
     sum: number1 + number2,
     mult: number1 * number2,
     div: Math.floor(number1 / number2),
     sub: number1 - number2,
-  };
 });
 
 const arrayGenerator = (type, object) => {
